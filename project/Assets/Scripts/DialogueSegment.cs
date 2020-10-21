19384ce53c4c4f4d4e503f641b8728c97eb25ba0
Mitchell Bonham <mitchell.r.bonham@gmail.com>
2020-10-22 10:48:44 +1100

    [#20] End of segment actions
    Show cards, start a new segment and clear all boxes onscreen

--- project/Assets/Scripts/DialogueSegment.cs
+++ project/Assets/Scripts/DialogueSegment.cs
@@ -4,15 +4,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum EndActions
+{
+    ShowCards,
+    StartSegment,
+    End
+}
+
 [RequireComponent(typeof(DialogueRenderer))]
 public class DialogueSegment : MonoBehaviour
 {
+    public string textTag;
     public TextBox[] textBoxes;
 
     public KeyCode advanceKey = KeyCode.Return;
+    public EndActions action;
+
+    public string nextSegment;
 
     private DialogueRenderer dialogueRenderer;
-
     private int position;
 
     // Start is called before the first frame update
@@ -41,13 +51,60 @@ public class DialogueSegment : MonoBehaviour
     {
         if (position < textBoxes.Length)
         {
+            // Advance to next box
             dialogueRenderer.ShowTextBox(ref textBoxes[position]);
             position++;
         }
         else
         {
-            // Prompt player for cards
-            this.enabled = false;
+            OnTextEnd();
         }
+    }
+
+    // Action taken when text finishes
+    void OnTextEnd()
+    {
+        switch (action)
+        {
+            case EndActions.ShowCards:
+                GetPlayerCard();
+                break;
+
+            case EndActions.StartSegment:
+                StartNextSegment();
+                break;
+
+            case EndActions.End:
+                dialogueRenderer.ClearBoxes();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    // Prompt player for cards
+    void GetPlayerCard()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player");
+        var playerCards = player.GetComponent<PlayerCardManager>();
+        playerCards.DisplayCards();
+    }
+
+    // Start a new segment with the nextSegment string
+    void StartNextSegment()
+    {
+        var allSegments = gameObject.GetComponents<DialogueSegment>();
+        foreach (var s in allSegments)
+        {
+            if (s.textTag == nextSegment)
+            {
+                s.enabled = true;
+                this.enabled = false;
+                return;
+            }
+        }
+        // Log if the next segment is not found
+        Debug.LogWarning("Next segment " + nextSegment + " not found");
     }
 }
