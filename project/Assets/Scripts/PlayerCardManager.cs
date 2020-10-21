d5c94acae996ffebf5f9e2f62dfe2a4c63afd264
Mitchell Bonham <mitchell.r.bonham@gmail.com>
2020-10-21 12:16:10 +1100

    [#20] Made cards display on screen

--- project/Assets/Scripts/PlayerCardManager.cs
+++ project/Assets/Scripts/PlayerCardManager.cs
@@ -20,16 +20,64 @@ public class PlayerCardManager : MonoBehaviour
     // List of all cards
     public List<Card> allCards;
 
+    [Space]
+
+    // Card prefab
+    public GameObject cardPrefab;
+
+    private GameObject canvas;
+
+    private List<GameObject> displayedCards;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Get canvas gameobject to display cards on
+        canvas = GameObject.FindGameObjectWithTag("uiCanvas");
+
+        availableCards = new List<Card>();
+
+        // Display cards -TEST-
+
+        foreach (var card in allCards)
+        {
+            availableCards.Add(card);
+        }
+
+        DisplayCards();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Display cards on screen
+    public void DisplayCards()
+    {
+        int offset = 0;
+
+        // Create card objects
+        foreach (var card in availableCards)
+        {
+            // Create and position card
+            var newCard = Instantiate(cardPrefab, canvas.transform);
+            newCard.transform.position = new Vector3(400 + offset, 150, 0);
+            offset += 200;
+
+            // Set card data
+            newCard.name = card.name;
+            newCard.GetComponentInChildren<Text>().text = card.name;
+            newCard.GetComponent<Image>().sprite = card.image;
+            // TODO: Add OnClick event
+        }
+    }
+
+    // 
+    public void SelectCard(string name)
+    {
+        // Destroy card objects
     }
 }
