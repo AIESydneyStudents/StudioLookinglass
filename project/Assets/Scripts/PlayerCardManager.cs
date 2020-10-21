4c46c4c03ef58014047eb7b1b861fd5ee107b666
Mitchell Bonham <mitchell.r.bonham@gmail.com>
2020-10-21 11:45:15 +1100

    [#20] Created card manager script

---
+++ project/Assets/Scripts/PlayerCardManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public struct Card
+{
+    public string name;
+    public Sprite image;
+}
+
+[DisallowMultipleComponent]
+public class PlayerCardManager : MonoBehaviour
+{
+    // List of cards the player can use
+    private List<Card> availableCards;
+
+    // List of all cards
+    public List<Card> allCards;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
