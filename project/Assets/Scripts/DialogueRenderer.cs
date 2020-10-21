4379e6ee28abdb4163290b9df766ab58fe918aee
Mitchell Bonham <mitchell.r.bonham@gmail.com>
2020-10-22 09:53:12 +1100

    [#20] Textbox renderer class
    Mostly a rewrite of the old manager's methods that showed boxes on screen

--- project/Assets/Scripts/DialogueRenderer.cs
+++ project/Assets/Scripts/DialogueRenderer.cs
@@ -1,19 +1,77 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public struct TextBox
+{
+    [TextArea] public string text;
+    public Sprite boxSprite;
+    public TextBox(string _text, Sprite _sprite)
+    {
+        text = _text;
+        boxSprite = _sprite;
+    }
+}
 
 [DisallowMultipleComponent]
 public class DialogueRenderer : MonoBehaviour
 {
+    public GameObject textboxPrefab;
+
+    public int maxActiveBoxes;
+
+    public Vector2 spawnPosition;
+
+    public float textboxMovement;
+
+    private GameObject canvas;
+    private List<GameObject> activeBoxes;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        activeBoxes = new List<GameObject>();
+        canvas = GameObject.FindGameObjectWithTag("uiCanvas");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowTextBox(string text)
     {
-        
+        TextBox t = new TextBox(text, null);
+        ShowTextBox(ref t);
+    }
+
+    public void ShowTextBox(ref TextBox box)
+    {
+        // Move existing boxes
+        foreach (var b in activeBoxes)
+        {
+            b.transform.Translate(0, textboxMovement, 0);
+        }
+
+        // Create new box
+        var newBox = Instantiate(textboxPrefab, canvas.transform);
+        newBox.transform.position = new Vector3(spawnPosition.x, spawnPosition.y, 0);
+        newBox.GetComponentInChildren<Text>().text = box.text;
+        newBox.GetComponent<Image>().sprite = box.boxSprite;
+        activeBoxes.Add(newBox);
+
+        // Remove old box past maximum
+        if (activeBoxes.Count > maxActiveBoxes)
+        {
+            Destroy(activeBoxes[0]);
+            activeBoxes.RemoveAt(0);
+        }
+    }
+
+    public void ClearBoxes()
+    {
+        foreach (var b in activeBoxes)
+        {
+            Destroy(b);
+        }
+        activeBoxes.Clear();
     }
 }
--- project/Assets/Scripts/DialogueSegment.cs
+++ project/Assets/Scripts/DialogueSegment.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(DialogueRenderer))]
 public class DialogueSegment : MonoBehaviour
 {
+    private DialogueRenderer dialog;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,11 @@ public class DialogueSegment : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    private void OnEnable()
     {
         
     }
