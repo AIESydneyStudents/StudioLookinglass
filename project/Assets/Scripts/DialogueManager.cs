30c422994b8c5f18f0fcafc00aee233b7faed1e6
Mitchell Bonham <mitchell.r.bonham@gmail.com>
2020-10-21 14:06:03 +1100

    [#20] Created NPC Dialogue script stubs

---
+++ project/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent]
+public class DialogueManager : MonoBehaviour
+{
+    // List of text scripts
+    private List<TextDisplay> displays;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get text scripts in object
+        gameObject.GetComponents<TextDisplay>(displays);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
