74692ea3488ad15f22f0c780d210d6e0157f168b
Mitchell Bonham <mitchell.r.bonham@gmail.com>
2020-10-30 09:12:11 +1100

    [#8] Rotate player movement to match camera angle

--- project/Assets/Scripts/PlayerMovement.cs
+++ project/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     public string sideAxis;
 
     public float movementSpeed;
+    public float angle;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +30,8 @@ public class PlayerMovement : MonoBehaviour
 
         // Move player
         Vector3 displacement = new Vector3(playerInput.x, 0, playerInput.y);
+        Quaternion rotation = Quaternion.Euler(0, angle, 0);
+        displacement = rotation * displacement;
         transform.position += displacement * Time.deltaTime * movementSpeed;
 
         // Rotate to face direction of movement
