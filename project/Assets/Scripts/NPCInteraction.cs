9a02eca94a4d49771d4057fbe9677669927b481e
Mitchell Bonham <mitchell.r.bonham@gmail.com>
2020-10-29 15:20:29 +1100

    [#10] NPC interaction starts dialogue

--- project/Assets/Scripts/NPCInteraction.cs
+++ project/Assets/Scripts/NPCInteraction.cs
@@ -6,19 +6,33 @@ public class NPCInteraction : MonoBehaviour
 {
     private GameObject player;
 
-    public string segment;
+    public string segmentName;
+
+    public float interactionDistance;
 
     public KeyCode interactionKey = KeyCode.E;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Check if player is close enough and has pressed key
+        if (Vector3.Distance(transform.position, player.transform.position) < interactionDistance && Input.GetKeyDown(interactionKey))
+        {
+            // Start segment
+            var allSegments = gameObject.GetComponents<DialogueSegment>();
+            foreach (var segment in allSegments)
+            {
+                if (segment.textTag == segmentName)
+                {
+                    segment.enabled = true;
+                }
+            }
+        }
     }
 }
