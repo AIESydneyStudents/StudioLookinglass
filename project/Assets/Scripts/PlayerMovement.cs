4475794b9310b4c9ca2b0184e8c357d7de0fe24b
Mitchell Bonham <mitchell.r.bonham@gmail.com>
2020-10-29 11:41:31 +1100

    [#8] Player movement rotation

---
+++ project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Transform mainCamera;
+
+    public string forwardAxis;
+    public string sideAxis;
+
+    public string movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis(forwardAxis) > 0)
+        {
+            transform.rotation = mainCamera.rotation;
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        }
+        else if (Input.GetAxis(forwardAxis) < 0)
+        {
+            transform.rotation = mainCamera.rotation;
+            transform.Rotate(0, 180, 0);
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        }
+
+        if (Input.GetAxis(sideAxis) > 0)
+        {
+            transform.rotation = mainCamera.rotation;
+            transform.Rotate(0, 90, 0);
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        }
+        else if (Input.GetAxis(sideAxis) < 0)
+        {
+            transform.rotation = mainCamera.rotation;
+            transform.Rotate(0, -90, 0);
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        }
+    }
+}
