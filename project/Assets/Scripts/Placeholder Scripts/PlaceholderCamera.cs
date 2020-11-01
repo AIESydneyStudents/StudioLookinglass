7adf789b6b9dc5fc65a300e2c0563bdafc2edb8d
Alex Smits <alexsmits247@gmail.com>
2020-11-01 17:51:27 +1100

    Placeholder Bobbing / camera follow
    Made some placeholder additions to add atmosphere

---
+++ project/Assets/Scripts/Placeholder Scripts/PlaceholderCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlaceholderCamera : MonoBehaviour
+{
+    public Transform PlayerTransform;
+
+    private Vector3 _cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+
+    void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+    }
+
+}
