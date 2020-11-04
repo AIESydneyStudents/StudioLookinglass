cbe94ef7357dfe158973132f01941930e6aae9bf
Mitchell Bonham <mitchell.r.bonham@gmail.com>
2020-11-05 10:11:50 +1100

    [#129] Choose from cards displayed on screen
    Adds chosen card to player

--- project/Assets/Scripts/CardChoice.cs
+++ project/Assets/Scripts/CardChoice.cs
@@ -10,7 +10,7 @@ public class CardChoice : MonoBehaviour
     public List<Card> cards;
 
     public float height;
-    public float xOffset;
+    public float deltaX;
 
     private List<GameObject> displayedCards;
 
@@ -18,20 +18,65 @@ public class CardChoice : MonoBehaviour
 
     private GameObject canvas;
 
+    private PlayerCardManager playerManager;
+
     private void Awake()
     {
         canvas = GameObject.FindGameObjectWithTag("uiCanvas");
         centre = canvas.GetComponent<RectTransform>().rect.width / 2;
+
+        playerManager = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCardManager>();
     }
 
+    // Display cards on screen
     public void DisplaySelection()
     {
         Vector2 position = new Vector2(centre, height);
 
+        float offset = 0;
+
+        displayedCards = new List<GameObject>();
+
         // Create card UI objects
         foreach (var card in cards)
         {
+            // Create and position card
+            var newCard = Instantiate(cardPrefab, canvas.transform);
+            newCard.transform.position = new Vector3(position.x + offset, position.y, 0);
+            offset += deltaX;
+            if ((offset / deltaX) % 2 == 0)
+            {
+                offset -= deltaX;
+                offset *= -1;
+            }
 
+            // Set card data
+            newCard.name = card.name;
+            newCard.GetComponent<Image>().sprite = card.image;
+            newCard.GetComponent<Button>().onClick.AddListener(delegate { SelectCard(card.name); });
+
+            // Add to list of displayed cards
+            displayedCards.Add(newCard);
+        }
+    }
+
+    public void SelectCard(string name)
+    {
+        // Clear UI
+        foreach (var card in displayedCards)
+        {
+            Destroy(card);
+        }
+        displayedCards.Clear();
+
+        // Add card to player's card manager
+        foreach (var card in cards)
+        {
+            if (card.name == name)
+            {
+                playerManager.AddNewCard(card);
+                break;
+            }
         }
     }
 }
