ff2b0d984acb49fb33a685e961a07fbd7e875447
Mitchell Bonham <mitchell.r.bonham@gmail.com>
2020-11-05 09:30:41 +1100

    [#129] Created card choice script

---
+++ project/Assets/Scripts/CardChoice.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardChoice : MonoBehaviour
+{
+    public GameObject cardPrefab;
+
+    public List<Card> cards;
+
+    public float height;
+    public float xOffset;
+
+    private List<GameObject> displayedCards;
+
+    private float centre;
+
+    private GameObject canvas;
+
+    private void Awake()
+    {
+        canvas = GameObject.FindGameObjectWithTag("uiCanvas");
+        centre = canvas.GetComponent<RectTransform>().rect.width / 2;
+    }
+
+    public void DisplaySelection()
+    {
+        Vector2 position = new Vector2(centre, height);
+
+        // Create card UI objects
+        foreach (var card in cards)
+        {
+
+        }
+    }
+}
