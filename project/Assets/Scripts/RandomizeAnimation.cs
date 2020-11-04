126ed664bc3b3dc7bca64b2f8ee1f1d0ca8ac39d
Caerwyn Bartley <c_barts@live.com>
2020-11-04 11:35:17 +1100

    Randomized Shade Bobbing

---
+++ project/Assets/Scripts/RandomizeAnimation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeAnimation : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.Play(0, -1, Random.value);
+    }
+}
