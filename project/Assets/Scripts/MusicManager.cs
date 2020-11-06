289c64dc628efbbe1a51162df381e877c4cb21fd
Mitchell Bonham <mitchell.r.bonham@gmail.com>
2020-11-06 12:59:55 +1100

    [#69] Music manager plays list of music
    Not tested

--- project/Assets/Scripts/MusicManager.cs
+++ project/Assets/Scripts/MusicManager.cs
@@ -2,17 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Song
+{
+    public AudioClip clip;
+    public uint BPM;
+}
+
+[RequireComponent(typeof(AudioSource))]
 public class MusicManager : MonoBehaviour
 {
+    public List<Song> playlist;
+    private Song nowPlaying;
+    private AudioSource source;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        source = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!source.isPlaying)
+        {
+            PlaySong();
+        }
+    }
+
+    public void PlaySong()
+    {
+        source.Stop();
+        int index = Random.Range(0, playlist.Count);
+        nowPlaying = playlist[index];
+        source.clip = nowPlaying.clip;
+        source.Play();
     }
 }
