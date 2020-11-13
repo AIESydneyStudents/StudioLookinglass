6441dcf2bf7350ded1bb49fe2501bfa380385f60
Mitchell Bonham <mitchell.r.bonham@gmail.com>
2020-11-13 14:38:41 +1100

    [#114] Created shade that can be set to music tempo

---
+++ project/Assets/Scripts/DancingAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DancingAnimation : MonoBehaviour
+{
+    public float animSpeed;
+    [Range(0, 1)] public float variation;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        anim.speed = animSpeed;
+        anim.Play(0, -1, 0);
+    }
+
+}
