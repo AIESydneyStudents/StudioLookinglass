d284a67674a6fb18794d03ea5a3c0c4d3a88c1b5
Mitchell Bonham <mitchell.r.bonham@gmail.com>
2020-11-20 14:41:35 +1100

    Added script to turn animator params on then off

---
+++ project/Assets/Scripts/AnimatorToggle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Param
+{
+    public Animator anim;
+    public string parameter;
+}
+
+public class AnimatorToggle : MonoBehaviour
+{
+    public Param[] @params;
+
+    public void StartTimer(float delay)
+    {
+        StartCoroutine(AnimTimer(delay));
+    }
+
+    public void Stop()
+    {
+        StopAllCoroutines();
+    }
+
+    // Set parameters to true for a time then set to false
+    private IEnumerator AnimTimer(float time)
+    {
+        // Set parameters to true
+        foreach (var p in @params)
+        {
+            p.anim.SetBool(p.parameter, true);
+        }
+
+        yield return new WaitForSeconds(time);
+
+        // Set parameters to false
+        foreach (var p in @params)
+        {
+            p.anim.SetBool(p.parameter, false);
+        }
+    }
+}
