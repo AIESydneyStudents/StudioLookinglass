cdabf1d76b37aa50d325d6f094785fb32820e2af
Barty <c_barts@live.com>
2020-11-20 19:44:16 +1100

    Added more props into scene and created materials for them

---
+++ project/Assets/Scripts/RandomizeBlendshape.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeBlendshape : MonoBehaviour
+{
+    SkinnedMeshRenderer SKM;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SKM = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    void Start()
+    {
+        SKM.SetBlendShapeWeight(0, Random.Range(0, 100));
+        SKM.SetBlendShapeWeight(1, Random.Range(0, 100));
+        SKM.SetBlendShapeWeight(2, Random.Range(0, 100));
+        SKM.SetBlendShapeWeight(3, Random.Range(0, 100));
+    }
+}
---
+++ project/Assets/Scripts/ShrinkMask.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkMask : MonoBehaviour
+{
+    public string Parameter;
+    public Animator MaskAnimator;
+
+    private bool inTrigger = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+
+    private void Update()
+    {
+        if (inTrigger == true)
+            MaskAnimator.SetBool(Parameter, false);
+        else
+            MaskAnimator.SetBool(Parameter, true);
+    }
+}
